2170de600d80e054e9b84ffe3f41df6d7d45e02f
Ben Shuan <benshuan@gmail.com>
2024-01-25 08:16:22 +0200

    Q1 finished

--- Tar 2/Tar 2 - basic react/src/ClassComp/CCColor.jsx
+++ Tar 2/Tar 2 - basic react/src/ClassComp/CCColor.jsx
@@ -1,21 +1,45 @@
-import  { Component } from 'react'
+import { Component } from 'react'
 
 export default class CCColor extends Component {
   constructor(props) {
     super(props)
 
+    this.containerStyle = {
+      display: "flex",
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      flexFlow: 'wrap',
+      padding: 10
+    }
+
+    this.buttonStyle = {
+      fontFamily: 'cursive',
+      fontWeight: 800,
+      fontSize: '1.3rem',
+      margin:4
+
+    }
+
     this.state = {
-      color:'white'
+      color: ''
     }
   }
 
-  colors = ['red','green','yellow','blue','pink','grey','purple','orange']
+  colors = ['red', 'green', 'yellow', 'blue', 'pink', 'black', 'purple', 'orange']
+
+
+
+  chngColor = (e) => {
+    this.setState({
+      color: e.target.value,
+    })
+  }
 
   render() {
     return (
-      <div style={{backgroundColor:this.state.color}}>
-        
-        {this.colors.map(col => <span style={{color:col}} key={col}> {col} </span>)}
+      <div style={{ ...this.containerStyle, backgroundColor: this.state.color }}>
+
+        {this.colors.map(col => <input type="button" onClick={this.chngColor} style={{ color: col,...this.buttonStyle }} className='button-29'  key={col} value={col} />)}
 
       </div>
     )
