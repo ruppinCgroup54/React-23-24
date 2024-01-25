29f511f7ccf61e7a2084d49d039b8554616c0366
RoniYehoshua <roni6531607@gmail.com>
2024-01-25 15:40:01 +0200

    Q2

--- Tar 2/Tar 2 - basic react/src/App.jsx
+++ Tar 2/Tar 2 - basic react/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import CCColor from './ClassComp/CCColor'
+import CCPsychometric from './ClassComp/CCPsychometric'
 
 function App() {
 
   return (
     <>
       <CCColor></CCColor>
+      <br />
+      <CCPsychometric></CCPsychometric>
     </>
   )
 }
